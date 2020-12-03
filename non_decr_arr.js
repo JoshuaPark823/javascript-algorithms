e4328c10bb205d2206da9a2483bea205193c9784
Josh Park <joshuapark823@gmail.com>
2020-12-03 10:10:26 -0500

    Second attempt

--- non_decr_arr.js
+++ non_decr_arr.js
@@ -12,8 +12,45 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var checkPossibility = function(nums) {
+const checkPossibility = (nums) => {
     
-    
+    /**
+     * logic:
+     * initialize a flag
+     * iterate through, if we see that a[i] > a[i + 1], then toggle the flag
+     * keep iterating, 
+     * if we find another, it'll enter the flag conditional
+     * if the flag had already been toggled before, return false
+     * if the flag is ONLY toggled once, return true
+     * 
+     * 
+     * is there a way we can just break out of the loop and return false if its already set?
+     */
 
+    let flag = 0;
+
+    // goes from a[0] -> a[l-2] (second last)
+    for (let i = 0; i <= nums.length - 2; i++) {
+
+        if (nums[i] > nums[i + 1]) {
+            flag++;
+        }
+    }
+
+    if (flag > 1) {
+        return false;
+    }
+    else {
+        if (flag > 0) {
+            return true;
+        }
+    }
 };
+
+// TESTING
+console.log(checkPossibility(([4,2,3])));
+console.log(checkPossibility(([4,2,1])));
+console.log(checkPossibility(([3,4,2,3]))); //shit need to check all previous, cuz if we change 4, n[1] still > than 2
+
+
+
