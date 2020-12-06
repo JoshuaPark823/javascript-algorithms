b29ec32787b9061db06622fcb68617b87205e9d3
Josh Park <joshuapark823@gmail.com>
2020-12-05 19:37:12 -0500

    O(logn) time complexity, ES6

--- is_palindrome.js
+++ is_palindrome.js
@@ -11,6 +11,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    
+const isPalindrome = (s) => {
+
+    // ignore non-alphanumeric and case
+    s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+
+    // two iterators, one from the front and one from the back closing in
+    for(let front = 0, back = s.length - 1; front < back; front++, back--) {
+        if (s[front] != s[back]) {
+            return false;
+        }
+    }
+
+    return true;
 };
+
+// Unit Testing
+const unit_testing = () => {
+    let str = ["hannah", "hello", "palindrome","1221", "mom", "racecar", "race a car", "A man, a plan, a canal: Panama"];
+    str.forEach(string => console.log(isPalindrome(string)));
+}
+unit_testing(); 
