7e3d4e320577e5e0b95bff8ad0924463bdc40559
Josh Park <jpark@explorance.com>
2020-12-27 16:45:34 -0500

    init commit

---
+++ matrix_block_sum.js
@@ -0,0 +1,27 @@
+/**
+ * Given a m * n matrix mat and an integer K, return a matrix answer where each 
+ * answer[i][j] is the sum of all elements mat[r][c] 
+ * for i - K <= r <= i + K, j - K <= c <= j + K, and (r, c) is a valid position 
+ * in the matrix.
+ * 
+ * Example 1:
+ * Input: mat = [[1,2,3],[4,5,6],[7,8,9]], K = 1
+ * Output: [[12,21,16],[27,45,33],[24,39,28]]
+ * 
+ * Constraints:
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n, K <= 100
+ * 1 <= mat[i][j] <= 100
+ */
+
+/**
+ * @param {number[][]} mat
+ * @param {number} K
+ * @return {number[][]}
+ */
+var matrixBlockSum = function(mat, K) {
+
+    
+    
+};
